778ea5efe2d1737a95d738cc32808f3a03a5cb75
David Galloway <DGalloway@alpari.co.uk>
2014-07-03 10:02:54 +0100

    Moved RedisConfig project to main solution
    created launch and kill powershell scripts for Redis as a stopgap measure

---
+++ qdf.AcceptanceTests/Alpari.QualityAssurance.RedisConfig/Purpose.cs
@@ -0,0 +1,9 @@
+﻿namespace Alpari.QualityAssurance.RedisConfig
+{
+    /// <summary>
+    /// This project exists solely to put a valid redis.windows.conf file in the $(SolutionDir)\packages\Redis-xxxx folder
+    /// </summary>
+    class Purpose
+    {
+    }
+}
