b28fc745575ee2b717d63d000f1bededabaf1d54
David Galloway <DGalloway@alpari.co.uk>
2014-07-03 09:18:15 +0100

    Final look at using Python Tools

--- RedisOnWindows.SineWave/UploadData/UploadData.py
+++ RedisOnWindows.SineWave/UploadData/UploadData.py
@@ -3,3 +3,23 @@ import redis, csv
 r = redis.StrictRedis()
 
 r.delete('cities')
+
+#readColsAlready = False
+#colCount = 0
+
+csvFile = csv.reader(open('cnx-deals-data.csv','rb'),delimiter='|')
+for row in csvFile:
+    #key = float(row[0])
+    key = float(row.pop(0)) # works! 
+    #key = row.pop(0)
+    #for x in range(1,row
+    #if readColsAlready != True:
+    #    readColsAlready = True
+    #    for col in row:
+    #        colCount +=1
+    #for x in range(1,colCount):
+    #dataSource = row
+    #for col in dataSource
+
+    data = ",".join(row)
+    r.zadd('cities',key,data)
