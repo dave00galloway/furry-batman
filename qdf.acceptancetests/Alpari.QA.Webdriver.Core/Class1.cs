85c2cdc6ec48342217175d1c6e319f881c4ffd56
DGalloway@alpari.co.uk <DGalloway@alpari.co.uk>
2014-12-02 17:28:50 +0000

    updated all NuGetPackages across solution
    added a skeleton Webdriver project

---
+++ qdf.acceptancetests/Alpari.QA.Webdriver.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alpari.QA.Webdriver.Core
+{
+    public class Class1
+    {
+    }
+}
